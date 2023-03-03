5092b3b6982cdd73bf59b04f55a2209cf18f23ab
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-03-03 18:09:49 +0100

    Add files via upload

---
+++ software/php/download_system_sort.php
@@ -0,0 +1,17 @@
+<?php
+
+$dir = "../files/system_sort/setup.exe";
+$filename = "setup.exe";
+
+
+header("Cache-Control: public");
+header("Content-Description: FIle Transfer");
+header("Content-Disposition: attachment; filename=$filename");
+header("Content-Type: application/zip");
+header("Content-Transfer-Emcoding: binary");
+
+readfile($dir);
+
+//redirect
+header("Location:../index.html");
+?>
